8d7449f59a51ee4654090c6078196431d959575b
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2023-03-19 01:56:35 +0530

    CREATE: Responsive navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,44 +1,47 @@
-import React from 'react'
-import './Navbar.css'
-// import toggle from '../../img/togglelight.png';
-// import togglemenu from '../../img/toggle.png';
+import React, { useState } from 'react';
+import './Navbar.css';
 import { Link } from 'react-scroll';
-// import { useState } from 'react';
+import { Icon } from '@iconify/react';
 import bjlogo from '../../img/bjlogo.jpg';
 
 function Navbar() {
-  // const[active,setActive]=useState("nav_menu");
-  // const toggleNav=()=>{
-  // active==="nav_menu" ? setActive("nav_menu nav_active"): setActive("nav_menu"); 
-  // }
-  // const [theme, setTheme] = useState('light');
+  const [showLinks, setShowLinks] = useState(false);
 
-  // const toggleTheme = () => {
-  //   setTheme(theme === 'light' ? 'dark' : 'light');
-  // };
-  // console.log(theme);
   return (
-
     <div>
       <div className="navbar">
         <div className="left">
           <img src={bjlogo} alt="" />
         </div>
 
-        <div className="right" style={{ sticky: true }} >
+        <div
+          className={showLinks ? 'mobile-navbar right' : 'right'}
+          style={{ sticky: true }}
+        >
           <ul>
-            <Link spy={true} to='Home' smooth={true} activeClass="activeClass"><li>Home</li></Link>
-            <Link spy={true} to='About' smooth={true} ><li>About</li></Link>
-            <Link spy={true} to='Services' smooth={true} ><li>Skills</li></Link>
-            <Link spy={true} to='Projects' smooth={true} ><li>Projects</li></Link>
-            <Link spy={true} to='Contact' smooth={true} ><li>Contact</li></Link>
-            {/* <div className="togglemenu" onClick={toggleNav}><img src={togglemenu} alt="" /></div> */}
+            <Link spy={true} to="Home" smooth={true} activeClass="activeClass">
+              <li>Home</li>
+            </Link>
+            <Link spy={true} to="About" smooth={true}>
+              <li>About</li>
+            </Link>
+            <Link spy={true} to="Services" smooth={true}>
+              <li>Skills</li>
+            </Link>
+            <Link spy={true} to="Projects" smooth={true}>
+              <li>Projects</li>
+            </Link>
+            <Link spy={true} to="Contact" smooth={true}>
+              <li>Contact</li>
+            </Link>
           </ul>
+          <div className={showLinks ? 'hamburger-menu-2' : 'hamburger-menu'}>
+            <Icon icon ="game-icons:hamburger-menu"  onClick={() => setShowLinks(!showLinks)}/>
+          </div>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
 
 export default Navbar;
